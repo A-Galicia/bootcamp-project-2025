c2844f83136aa241dc755fc65d79ccd7c42f0f88
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2025-10-19 11:59:00 -0700

    feat: added portfolio component

--- bootcamp-milestone-2/src/components/Portfolio.tsx
+++ bootcamp-milestone-2/src/components/Portfolio.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
-import style from './Footer.module.css';
+import style from './Portfolio.module.css';
 
 export default function Footer() {
   return (
-    <footer className={style.footer}>
-      © 2025 Alfredo&apos;s Portfolio | All Rights Reserved
-    </footer>
+    <div>
+      <div className={style.project}>
+        <p>Strength Projector</p>
+        <a
+          href='https://strength-projector.pages.dev/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img
+            src='../public/StrengthProjector.png'
+            alt='Home page of my Strength Projector web application'
+          />
+        </a>
+      </div>
+
+      <div className={style.project}>
+        <p>Asteroids</p>
+        <a
+          href='https://github.com/A-Galicia/Astroids-Game'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img
+            src='../public/AsteroidsScreenShot.png'
+            alt='In-game image of my rendition of the Asteroids arcade game'
+          />
+        </a>
+      </div>
+
+      <div className={style.project}>
+        <p>Github</p>
+        <a
+          href='https://github.com/A-Galicia'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <img
+            src='public/GithubScrn.png'
+            alt='Home page of my GitHub profile'
+          />
+        </a>
+      </div>
+    </div>
   );
 }
