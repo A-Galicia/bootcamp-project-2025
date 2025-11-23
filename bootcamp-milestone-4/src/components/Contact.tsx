fcd7d57e5ca647982816550cae08063f5cc1da11
Alfredo <193377562+A-Galicia@users.noreply.github.com>
2025-11-22 22:33:55 -0800

    feat: added sending emails

--- bootcamp-milestone-4/src/components/Contact.tsx
+++ bootcamp-milestone-4/src/components/Contact.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+'use client';
+import { useRef, useState } from 'react';
 import style from './Contact.module.css';
 import Image from 'next/image';
+import emailjs, { send } from '@emailjs/browser';
+
+const SERVICE_ID = 'personalWebsite';
+const TEMPLATE_ID = 'template_bo8j33p';
+const PUBLIC_KEY = 'GaLWOX77XxeLiC-oT';
 
 export default function Contact() {
+  const form = useRef({} as HTMLFormElement);
+  const [sending, setSending] = useState(false);
+  const [sent, setSent] = useState(false);
+  const [error, setError] = useState('');
+
+  const sendEmail = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSending(true);
+
+    emailjs
+      .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, {
+        publicKey: PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          setSent(true);
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+          setError(error.text);
+        }
+      )
+      .finally(() => {
+        setSending(false);
+      });
+  };
+
   return (
     <div>
       <h1 className={style.pageTitle}>Contact</h1>
+
       <div className={style.aboutContact}>
         <Image
           src='/Gmail.png'
@@ -14,7 +49,30 @@ export default function Contact() {
           height={500}
           className={style.aboutImg}
         />
+
         <p className={style.aboutGmail}>alfgalic@gmail.com</p>
+
+        {!sending ? null : <p className={style.loading}>Sending...</p>}
+        {error ? <p className={style.error}>Error: {error}</p> : null}
+
+        {sent ? (
+          <p className={style.success}>Successfully sent message!</p>
+        ) : (
+          <form className={style.form} ref={form} onSubmit={sendEmail}>
+            <label className={style.label}>Name</label>
+            <input className={style.input} type='text' name='name' required />
+            <label className={style.label}>Email</label>
+            <input className={style.input} type='email' name='email' required />
+            <label className={style.label}>Message</label>
+            <textarea className={style.input} name='message' />
+            <input
+              className={style.submit}
+              type='submit'
+              value='Send'
+              required
+            />
+          </form>
+        )}
 
         <a
           href='https://www.linkedin.com/in/alfredo-galicia2/'
